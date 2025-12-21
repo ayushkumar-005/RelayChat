b04290539f3bc9a21e560764b3e17d52adbea1d0
Ayush Kumar <ayushkumarwk555@gmail.com>
2025-12-21 13:02:36 +0530

    Initial Commit

---
+++ frontend/src/App.jsx
@@ -0,0 +1,10 @@
+const App = () => {
+    return (
+        <div className="placeholder">
+            <h1>RelayChat</h1>
+            <p>Coming Soon</p>
+        </div>
+    );
+};
+
+export default App;
