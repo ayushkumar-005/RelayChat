e44815c670fe711a9828fe75f57c9b4bc5c2826f
Ayush Kumar <ayushkumarwk555@gmail.com>
2025-12-23 13:46:48 +0530

    Login Refactor

--- backend/src/controllers/auth.controller.js
+++ backend/src/controllers/auth.controller.js
@@ -71,6 +71,12 @@ export const signup = async (req, res) => {
 export const login = async (req, res) => {
     const { email, password } = req.body;
 
+    if (!email || !password) {
+        return res
+            .status(400)
+            .json({ message: "Email and password are required" });
+    }
+
     try {
         // Check if user mail exists
         const user = await User.findOne({ email });
